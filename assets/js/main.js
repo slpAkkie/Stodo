f9f5468c0bfc706352ec9c4adb4ef71605754c1a
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2021-03-21 18:58:50 +0300

    Update: popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /**
- * -------------------------
+ * --------------------------------------------------
  * JSON data */
 
 DATA = {
@@ -9,13 +9,13 @@ DATA = {
       title: 'Активные',
       items: [
         {
-          title: 'To-do title',
+          title: 'Сделать сайт со списком дел',
           done: false,
           until: '12-05-2021',
           childs: [
             {
-              title: 'To-do title',
-              done: false,
+              title: 'Придумать дизайн',
+              done: true,
             },
           ],
         },
@@ -25,18 +25,18 @@ DATA = {
       title: 'Завершенные',
       items: [
         {
-          title: 'To-do title',
+          title: 'Сверстать базовый шаблон',
           done: true,
           until: '12-05-2021',
           childs: null,
         },
         {
-          title: 'To-do title',
-          done: false,
+          title: 'Написать несколько функций',
+          done: true,
           until: '12-05-2021',
           childs: [
             {
-              title: 'To-do title',
+              title: 'Написать функции вставки задач из памяти',
               done: true,
             },
           ],
@@ -47,7 +47,13 @@ DATA = {
 };
 
 /**
- * -------------------------
+ * --------------------------------------------------
+ * Temp JSON data for popup */
+
+PPP_TMPDT = null;
+
+/**
+ * --------------------------------------------------
  * Tab creator
  * 
  * @param {Number} id
@@ -60,7 +66,7 @@ function createTab(id, title) {
 }
 
 /**
- * -------------------------
+ * --------------------------------------------------
  * Tab loader */
 
 function loadTabs() {
@@ -70,7 +76,7 @@ function loadTabs() {
 }
 
 /**
- * -------------------------
+ * --------------------------------------------------
  * Tab content creator
  * 
  * @param {Number} id
@@ -100,7 +106,52 @@ function createTodo(id, obj) {
 }
 
 /**
- * -------------------------
+ * --------------------------------------------------
+ * Editable subs creator
+ * 
+ * @param {Number} id
+ * @returns 
+ */
+
+function createEditableSub(id) {
+  let sub = createNode(`
+  <div data-sub-id="${id}" class="popup__todo-editable-child-container">
+    <div class="popup__todo-editable-child">
+      <input type="checkbox" class="popup__todo-editable-child-done">
+      <input type="text" class="popup__todo-editable-child-title">
+      <div class="ui ui-button ui-icon_delete popup__todo-editable-child-delete-button"></div>
+    </div>
+  </div>`);
+
+  // Default values
+  PPP_TMPDT.subs.push({title: '', done: false});
+
+  // Handler for checkbox
+  _(sub)._('.popup__todo-editable-child-done').on('click', function () {
+    PPP_TMPDT.subs[id].done = !PPP_TMPDT.subs[id].done;
+  });
+
+  // Handler for title change
+  _(sub)._('.popup__todo-editable-child-title').on('input', function () {
+    PPP_TMPDT.subs[id].title = this.value;
+  });
+
+  // Handler for remove
+  _(sub)._('.popup__todo-editable-child-delete-button').on('click', function () {
+    sub.remove();
+
+    for (let i = id; i < PPP_TMPDT.subs.length - 1; i++) {
+      PPP_TMPDT.subs[i] = PPP_TMPDT.subs[i + 1];
+      _(`[data-sub-id="${i + 1}"]`).setAttribute('data-sub-id', i);
+    }
+    PPP_TMPDT.subs.length--;
+  });
+
+  return sub;
+}
+
+/**
+ * --------------------------------------------------
  * Tab content loader 
  * 
  * @param {Array} todos
@@ -116,7 +167,7 @@ function loadTabContent(todos, where = null) {
 }
 
 /**
- * -------------------------
+ * --------------------------------------------------
  * DOM Elements creator
  * 
  * @param {String} markup
@@ -129,7 +180,7 @@ function createNode(markup) {
 }
 
 /**
- * -------------------------
+ * --------------------------------------------------
  * Init App */
 
 loadTabs();
@@ -137,12 +188,58 @@ _(`.tabs__tab[data-tab-id="0"]`).toggleClass('tabs__tab_active');
 loadTabContent(DATA.tabs[0].items);
 
 /**
- * -------------------------
+ * --------------------------------------------------
+ * Tab selector */
+
+function getCurrentTabItems() { return DATA.tabs[DATA.active_tab].items }
+
+/**
+ * --------------------------------------------------
  * Tab switcher */
 
-_('.tabs__tab').on('click', function () {
-  console.log(`Пользователь открыл вкладку с id=${DATA.active_tab = this.attributes['data-tab-id'].value}`);
+function switchTab() {
+  DATA.active_tab = this.getAttribute('data-tab-id');
+  console.log(`Пользователь открыл вкладку id(${DATA.active_tab})`);
   _(`.tabs__tab_active`)?.toggleClass('tabs__tab_active');
   this.toggleClass('tabs__tab_active');
-  loadTabContent(DATA.tabs[DATA.active_tab].items);
+  loadTabContent(getCurrentTabItems());
+}
+
+
+
+/**
+ * --------------------------------------------------
+ * Handlers */
+
+// --------------------------------------------------
+_('.tabs__tab').on('click', switchTab);
+
+// --------------------------------------------------
+_('#js-add-new').on('click', () => {
+  PPP_TMPDT = {
+    title: '',
+    done: false,
+    until: null,
+    subs: [],
+  };
+
+  _('.popup__wrapper').removeClass('popup__wrapper_closed');
+  _('.popup__wrapper').addClass('popup__wrapper_shown');
+});
+
+// --------------------------------------------------
+_('.popup__close-button').on('click', () => {
+  _('.popup__wrapper').removeClass('popup__wrapper_shown');
+  _('.popup__wrapper').addClass('popup__wrapper_closed');
+});
+
+// --------------------------------------------------
+_('#popup__add-child').on('click', function () {
+  PPP_TMPDT || (PPP_TMPDT = {
+    title: '',
+    done: false,
+    until: null,
+    subs: [],
+  });
+  _('.popup__childs').insert(createEditableSub(PPP_TMPDT.subs.length));
 });
