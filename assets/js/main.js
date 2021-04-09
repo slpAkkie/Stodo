6d584fcedb07737337630581e4e7f42c64da41e6
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2021-04-09 17:13:53 +0300

    Update: interact with popup
    Add ability to get data from popup
    Generating control button dynamicaly when popup is opening
    New support functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@
 
 const data = {
   active_tab: 0,
+  editID: null,
 
   get currentTab() {
     return this.active_tab ? 'completed' : 'active';
@@ -41,11 +42,17 @@ const task = {
 
   get containerEl() { return selectEl(this.container) },
 
-  create() { },
+  create() {
+    popup.mode = 'create';
+    popup.show();
+  },
 
   delete() { },
 
-  edit(todoEl) { },
+  edit() {
+    data.editID = this.dataID;
+    popup.show();
+  },
 
   setCompleted() { },
 
@@ -65,11 +72,11 @@ const task = {
         <div class="col">
           <input class="todo__completed-checkbox _mt-1" type="checkbox" ${completed ? 'checked' : ''}>
         </div>
-        <div class="_flex sm:col lg:row _grow _justify-between _ml-4">
+        <div class="todo__title-wrapper _flex sm:col lg:row _grow _justify-between _ml-4">
           <div class="todo__title">
             <span>${title}</span>
           </div>
-          <div class="todo__until-status sm:_mt-1 ${statusClass}">
+          <div class="todo__until-status _text-semi-bold sm:_mt-1 ${statusClass}">
             <span>${status}</span>
           </div>
         </div>
@@ -87,6 +94,8 @@ const task = {
     }
 
     todo.dataID = id;
+
+    selectEl('.todo__title-wrapper', todo).addEventListener('click', task.edit.bind(todo));
 
     return todo;
   },
@@ -125,12 +134,16 @@ function createEl(markup) {
   return template.content.firstChild;
 }
 
+function insertFirst(parent, child) {
+  parent.insertBefore(child, parent.firstChild)
+}
+
 function render() {
   let todosContainer = task.containerEl,
       todos = tabs[data.currentTab];
   todosContainer.innerHTML = null;
   if (!todos.length) todosContainer.append(createEl(`<h3 class="_text-center">В этом списке нет задач</h3>`));
-  else todos.forEach((todo, id) => todosContainer.insertBefore(task.render(todo, id), todosContainer.firstChild));
+  else todos.forEach((todo, id) => insertFirst(todosContainer, task.render(todo, id)));
 }
 
 function extractFilters() { }
@@ -151,19 +164,112 @@ function tabSwitch() {
 
 const popup = {
 
+  /**
+   * Can be 'create', 'edit'
+   * @var {string} define in which mode popup will be opened
+   */
+  mode: 'create',
+
+  subContainerSelector: '#js-subs-container',
+  emptySubContainerClass: 'popup__subs-container_empty',
+
+  get subContainerEl() { return selectEl(this.subContainerSelector) },
+
+  get subs() { return selectEl('.popup__sub', this.subContainerEl) },
+
   selector: '.popup',
 
   get el() { return selectEl(this.selector) },
 
-  show() { },
+  controlButtonsSelector: '.popup__control-button',
+
+  get controlButtons() { return selectEl(this.controlButtonsSelector) },
+
+  show() {
+    let editMode = this.mode === 'edit';
+
+    popup.reset();
+
+    let cancelButton = createEl(`<input type="button" value="Отменить" class="button popup__cancel">`);
+    this.controlButtons.append(cancelButton);
+    cancelButton.addEventListener('click', popup.hide.bind(popup));
+
+    if (editMode) {
+      let deleteButton = createEl(`<input type="button" value="Удалить" class="button button_danger popup__delete _ml-2">`);
+      this.controlButtons.append(deleteButton);
+      deleteButton.addEventListener('click', task.delete);
+    }
+
+    let saveButton = createEl(`<input type="button" value="${editMode ? 'Сохранить' : 'Создать'}" class="button button_success popup__save _ml-2">`);
+    this.controlButtons.append(saveButton);
+    saveButton.addEventListener('click', task.save);
+
+    addClass(this.el, '_shown');
+  },
 
   hide() {
+    data.editID = null;
     removeClass(this.el, '_shown');
   },
 
-  reset() { },
+  reset() {
+    selectEl('#js-popup-title').value = null;
+    selectEl('#js-popup-date').value = null;
+    selectEl('#js-popup-time').value = null;
+    selectEl('#js-popup-completed').checked = false;
+    selectEl('#js-subs-container').innerHTML = null;
+    addClass(selectEl('#js-subs-container'), this.emptySubContainerClass);
+
+    this.controlButtons.innerHTML = null;
+  },
 
   fill( data = {} ) { },
+
+  getData() {
+    let data = {until: {}};
+    data.title = selectEl('#js-popup-title').value;
+    data.until.date = selectEl('#js-popup-date').valueAsDate.toLocaleDateString();
+    data.until.time = selectEl('#js-popup-time').value;
+    data.completed = selectEl('#js-popup-completed').checked;
+    data.subs = this.getSubsData();
+
+    return data;
+  },
+
+  getSubsData() {
+    let data = [];
+    each(this.subs, el => {
+      data.push({
+        title: selectEl('.popup__sub-title', el).value,
+        completed: selectEl('.popup__sub-checkbox', el).checked,
+      })
+    });
+
+    return data;
+  },
+
+  addSub() {
+    let markup = `
+      <div class="popup__sub row _align-center">
+        <input type="checkbox" class="popup__sub-checkbox">
+        <input type="text" class="popup__sub-title input _grow _ml-4">
+        <span class="popup__sub-delete-button _ml-2 _font-small _text-danger _text-semi-bold">Удалить</span>
+      </div>
+    `;
+
+    let subContainer = this.subContainerEl;
+    removeClass(subContainer, this.emptySubContainerClass);
+    let sub = createEl(markup);
+    subContainer.append(sub);
+
+    selectEl('.popup__sub-delete-button', sub).addEventListener('click', this.removeSub);
+  },
+
+  removeSub() {
+    this.parentElement.remove();
+
+    if (!popup.subContainerEl.innerHTML) addClass(popup.subContainerEl, popup.emptySubContainerClass)
+  }
 
 };
 
@@ -251,10 +357,13 @@ window.addEventListener('DOMContentLoaded', function () {
       el => el.addEventListener('click', tabSwitch)
   );
 
-  /** Click on todo */
-  selectEl('.todos-container').addEventListener('click', task.edit);
-
   /** Close popup */
   selectEl('#js-popup-close').addEventListener('click', popup.hide.bind(popup));
+
+  /** Add sub task in popup */
+  selectEl('#js-add-sub').addEventListener('click', popup.addSub.bind(popup));
+
+  /** Open form to create new task */
+  selectEl('#js-todo-add').addEventListener('click', task.create.bind(task))
 
 });
