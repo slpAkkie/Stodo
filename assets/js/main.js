a868fcab8c72460ff5906a62677c93139230288a
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2021-04-09 14:21:18 +0300

    Update: render
    After page loading, the tab data will be loaded and rendered on the page.
    Added some new functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,10 @@
 
 const data = {
   active_tab: 0,
+
+  get currentTab() {
+    return this.active_tab ? 'completed' : 'active';
+  }
 };
 
 
@@ -14,29 +18,10 @@ const data = {
  * Tabs data
  */
 
-const tabs = {
+let tabs = {
 
   /** ------------------------- */
-  active: [
-    {
-      title: 'First todo',
-      until: {
-        date: '10.04.2021',
-        time: '11:00',
-      },
-      sub: [],
-    },
-    {
-      title: 'Second todo',
-      until: {
-        date: '12.04.2021',
-        time: '15:00',
-      },
-      sub: [
-        { title: 'First sub for second todo', completed: false }
-      ],
-    },
-  ],
+  active: [],
 
   /** ------------------------- */
   completed: [],
@@ -64,9 +49,54 @@ const task = {
 
   edit() { },
 
-  render() { },
+  calculateStatus() { return ['Скоро', '_text-green'] },
 
-  renderSub() { },
+  render(data = {}) {
+    let {title, completed, until, subs} = data;
+    let [status, statusClass] = this.calculateStatus(until);
+    let isSubs = subs.length;
+
+    let markup = `<div class="todo">
+      <div class="todo__inner row _align-center">
+        <div class="col">
+          <input class="todo__completed-checkbox" type="checkbox" ${completed ? 'checked' : ''}>
+        </div>
+        <div class="_flex sm:col lg:row _grow _justify-between _ml-4">
+          <div class="todo__title">
+            <span>${title}</span>
+          </div>
+          <div class="todo__until-status sm:_mt-1 ${statusClass}">
+            <span>${status}</span>
+          </div>
+        </div>
+      </div>
+      ${isSubs ? '<div class="todo__subs"></div>' : ''}
+    </div>`;
+
+    let todo = createEl(markup);
+
+    if (isSubs) {
+      let subContainer = selectEl('.todo__subs', todo);
+      subs.forEach(sub => {
+        subContainer.append(task.renderSub(sub));
+      });
+    }
+
+    return todo;
+  },
+
+  renderSub(data) {
+    let {title, completed} = data;
+
+    let markup = `<div class="sub-todo row _align-center">
+      <input class="todo__completed-checkbox" type="checkbox" ${completed ? 'checked' : ''}>
+      <div class="todo__title _ml-4">
+        <span>${title}</span>
+      </div>
+    </div>`;
+
+    return createEl(markup);
+  },
 
   move() { },
 
@@ -79,7 +109,18 @@ const task = {
  * General functions
  */
 
-function render() { }
+function createEl(markup) {
+  let template = document.createElement('template');
+  template.innerHTML = markup.trim();
+  return template.content.firstChild;
+}
+
+function render() {
+  let todosContainer = selectEl('.todos-container'),
+      todos = tabs[data.currentTab];
+  if (!todos.length) todosContainer.append(createEl(`<h3 class="_text-center">В этом списке нет задач</h3>`));
+  else todos.forEach(todo => todosContainer.insertBefore(task.render(todo), todosContainer.firstChild));
+}
 
 function extractFilters() { }
 
@@ -115,11 +156,17 @@ const popup = {
  * LocalStorage functions
  */
 
-const LS = {
+const ls = {
 
-  set( k, v ) { },
+  set( key, value ) {
+    localStorage.setItem(key, JSON.stringify({data: value}));
+    return value;
+  },
 
-  get( k, d = null ) { },
+  get( key, defaultValue = null ) {
+    let gotten = localStorage.getItem(key);
+    return gotten ? JSON.parse(gotten).data : (defaultValue || null);
+  },
 
 };
 
@@ -130,8 +177,8 @@ const LS = {
  * Additions
  */
 
-function selectEl(selector) {
-  let els = document.querySelectorAll(selector);
+function selectEl(selector, parent = document) {
+  let els = parent.querySelectorAll(selector);
   switch (els.length) {
     case 0: return null;
     case 1: return els[0];
@@ -170,9 +217,19 @@ function toggleClass(el, className) {
  * EventListeners
  */
 
-/** ---------------
- * Switch tab */
-each(
-    selectEl('.tab'),
-    el => el.addEventListener('click', tabSwitch)
-);
+window.addEventListener('DOMContentLoaded', function () {
+
+  let loadedTabs = ls.get('tabs');
+  if (!loadedTabs) ls.set('tabs', tabs);
+  else tabs = loadedTabs;
+
+  /** First todos render */
+  render();
+
+  /** Switch tab */
+  each(
+      selectEl('.tab'),
+      el => el.addEventListener('click', tabSwitch)
+  );
+
+});
